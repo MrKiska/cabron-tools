2e2be6c99944e6c7fd51a68ce36ec33df4505e94
InTostor <gena05irk@yandex.ru>
2022-12-13 21:38:46 +0800

    now works with text file list

--- entry.py
+++ entry.py
@@ -3,11 +3,9 @@ import time
 print("\nInitializing mierda checker v0.1.34-13-12-2022...")
 time.sleep(2)
 
-names = [
-    "John",
-    "Mao",
-    "Voldemar"
-]
+
+fileobj=open("names.txt")
+names=fileobj.read().split('\n')
 
 if __name__ == "__main__":
     stdscr = curses.initscr()
