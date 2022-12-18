9381dd0614e0b0d48e0e959a842f49b1446b9296
InTostor <gena05irk@yandex.ru>
2022-12-18 21:14:10 +0800

    supporting women

--- Cabron_generador/generator.py
+++ Cabron_generador/generator.py
@@ -5,11 +5,21 @@ first = open("first.txt").read().split('\n')
 second = open("second.txt").read().split('\n')
 third = open("third.txt").read().split('\n')
 
+fem = ["а","ь","я"]
+
+
+print("press enter")
 while True:
     if input()=="":
         name=[]
         name.append(first[randint(0,len(first))-1])
         name.append(second[randint(0,len(second)-1)])
         name.append(third[randint(0,len(third)-1)])
-        print(str(name))
+        if name[1][-1:] in fem:
+            name[0] = name[0][:-2]+"ая"
+        elif name[1][-2:]=="о":
+            name[0] = name[0][:-2]+"ое"
+
+
+        print(" ".join(str(i) for i in name))
         
