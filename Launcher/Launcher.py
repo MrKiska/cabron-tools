b032e700e8cb7e8103ed8eff94cdfbab6f8ec4a1
InTostor <gena05irk@yandex.ru>
2022-12-31 23:32:03 +0800

    Revert "moved to other branch"
    This reverts commit e73b0615697314e1e3db8532b0f0f3116aa38cd3.

--- Launcher/Launcher.py
+++ Launcher/Launcher.py
@@ -1,41 +1,37 @@
 import importlib
 import json
 import os
-from lib.voter import askForOptions
 
 root = os.getcwd()
 
 
 pkgListFile = open(root +"/Launcher/pkgList.json")
 greeting = open(root + "/resources/greeting.txt").read()
-
-packages = os.listdir("Packages")
-print(packages)
-pkgsInfo = []
-
-for i in range(0,len(packages)):
-
-    with open("Packages/"+packages[i]+"/info.json") as f:
-        moduleInfo = list(json.load(f).items())
-    pkgsInfo.append(moduleInfo)
-
-
+with pkgListFile as f:
+    pkgList = list(json.load(f).items())
 
 while True:
     try:
 
         print(greeting)
 
-        for i in range(0,len(pkgsInfo)):
-            info = [
-                str(i) + " - " + pkgsInfo[i][0][1], # name of package
-                "  * " + pkgsInfo[i][1][1]
-            ]
-            print("\n".join(info))
-        input(":")
+        for pkg in pkgList:
+            print(pkg[0],"-",pkg[1]['title'])
+            print("  *",pkg[1]['description'],"\n")
 
-        
-        # importlib.import_module(pkgPath)
+        print("Choose package or q to exit")
+        entered = input(":")
+        if entered=="q":
+            exit()
+        pkgChosen = int(entered)
+
+        pkgPath = pkgList[pkgChosen-1][1]['initPath']
+
+        pkgImport = pkgPath
+
+
+
+        importlib.import_module(pkgImport)
 
     except KeyboardInterrupt:
         print("exit")
