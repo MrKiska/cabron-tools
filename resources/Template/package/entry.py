22cb6c50b528cdb1097889a08365c9ff2ce534ec
InTostor <gena05irk@yandex.ru>
2023-01-13 22:56:34 +0800

    Interesting marijuana (v0.3a) on this version, launcher is stable enough to not fall after looking on it in next version there will be devkit and more commands

--- resources/Template/package/entry.py
+++ resources/Template/package/entry.py
@@ -2,7 +2,18 @@ packageInternalName = ""
 
 
 from importlib.machinery import SourceFileLoader
-import os
-pkgRoot = os.getcwd + "/Packages/" + packageInternalName
+import os, json
+
+
+pkgRoot = os.getcwd + "/Packages/" + packageInternalName # getting root of package
+
+with open(pkgRoot + "/info.json") as f: # getting package info
+    packageJson = json.load(f).items()
 
 mainFile = SourceFileLoader("main",pkgRoot+"/main.py").load_module()
+
+# there are preparings before start of main script
+
+mainFile.main(packageInternalName = packageInternalName) # loads main script
+
+# something to do after exec
---
+++ resources/Template/package/main.py
@@ -0,0 +1,8 @@
+
+import os
+pkgRoot = ""
+
+def main(packageInternalName):
+    pkgRoot = os.getcwd + "/Packages/" + packageInternalName
+    # something to do, maybe some preparings and while loop
+    pass
