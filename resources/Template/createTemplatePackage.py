d61f2a37ac3de07c4dbe69f78360e45904ce16e7
InTostor <gena05irk@yandex.ru>
2023-01-16 16:46:14 +0800

    now packages creating normally

--- resources/Template/createTemplatePackage.py
+++ resources/Template/createTemplatePackage.py
@@ -30,12 +30,21 @@ def main():
 
     with io.open(newPackageRoot + "/entry.py", 'r+') as file:
         efile = str(file.read())
-        print(efile)
+        file.close()
+    with io.open(newPackageRoot + "/entry.py", 'w') as file:
         efile = efile.replace('packageInternalName = ""','packageInternalName = "'+ inName +'"')
         file.truncate(0)
         file.write(efile)
         file.close()
     print("edited entry script")
+
+    # Achtung! This is the worst solution i made in my whole programming way. REWORK IT ASAP (or close eyes on it)
+
+    infoJson = "{\n\"name\": \"%s\",\n\"description\": \"%s\",\n\"version\": \"%s\",\n\"loadFile\": \"Packages.%s.entry\"\n}" % (norName,desc,version,inName)
+
+    with io.open(newPackageRoot + "/info.json", 'w') as jfile:
+        jfile.write(infoJson)
+        jfile.close()
 
     # replace {inName} in files with {inName} from dialogue
     # print info about it
